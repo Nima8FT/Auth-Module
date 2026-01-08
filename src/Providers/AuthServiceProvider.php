634a502403e1036eb4e8642fd1ef7efb28b7345a
Nima Malekoti <nima.malekoti@mondfx.com>
2026-01-08 13:01:35 +0330

    fix: auth providers

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -3,42 +3,42 @@
 namespace AuthModule\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    /**
+     * Register bindings and merge configs.
+     */
     public function register(): void
     {
         if (! class_exists(\Tymon\JWTAuth\JWTAuth::class)) {
             return;
         }
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/auth-module.php',
+            'auth-module'
+        );
     }
 
+    /**
+     * Bootstrap package services.
+     */
     public function boot(): void
     {
         if (! class_exists(\Tymon\JWTAuth\JWTAuth::class)) {
             return;
         }
 
-        $this->registerAuthGuard();
-        $this->registerRoutes();
-    }
-
-    protected function registerRoutes(): void
-    {
-        $this->loadRoutesFrom(
-            __DIR__.'/../../routes/api.php'
-        );
-    }
-
-    protected function registerAuthGuard(): void
-    {
-        if (! app()->configurationIsCached()) {
-            config([
-                'auth.guards.jwt' => [
-                    'driver' => 'jwt',
-                    'provider' => 'users',
-                ],
-            ]);
+        if ($this->app->bound('auth')) {
+            Auth::extend('jwt', function ($app, $name, array $config) {
+                return $app['tymon.jwt.auth'];
+            });
         }
+
+        $this->loadRoutesFrom(
+            __DIR__ . '/../../routes/api.php'
+        );
     }
 }
